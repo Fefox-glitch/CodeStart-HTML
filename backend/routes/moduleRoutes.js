3f4b44416896de805efb9daf863f7553eaa50968
CodeSandbox <support@codesandbox.io>
2025-06-18 05:04:40 +0000

    chore: remove redundant backend and frontend directories

--- backend/routes/moduleRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getModules, createModule } = require("../controllers/moduleController");
-
-router.get("/", getModules);
-router.post("/", createModule);
-
-module.exports = router;
